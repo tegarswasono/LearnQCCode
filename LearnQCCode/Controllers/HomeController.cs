a979f75d138ffd90f53fbde865911775a79ca295
tegarswasono <tegar.s@weefer.co.id>
2023-05-16 11:08:57 +0700

    add SmtpClient and send attachment to email

--- LearnQCCode/Controllers/HomeController.cs
+++ LearnQCCode/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Threading.Tasks;
 
 namespace LearnQCCode.Controllers
@@ -30,7 +32,7 @@ namespace LearnQCCode.Controllers
             QRCodeData QrCodeInfo = QrGenerator.CreateQrCode(TicketId, QRCodeGenerator.ECCLevel.Q);
             QRCode QrCode = new QRCode(QrCodeInfo);
             Bitmap QrBitmap = QrCode.GetGraphic(60);
-            byte[] BitmapArray = QrBitmap.BitmapToByteArray();
+            byte[] BitmapArray = BitmapToByteArray(QrBitmap);
             string QrUri = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(BitmapArray));
             ViewBag.QrCodeUri = QrUri;
             ViewBag.TicketId = TicketId;
@@ -39,24 +41,89 @@ namespace LearnQCCode.Controllers
 
         public IActionResult Privacy()
         {
+            SendEmail();
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        private void SendEmail()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            string SMTPServer = "smtp.googlemail.com";
+            int SMTPPort = 587;
+            string SMTPUser = "";
+            string SMTPPassword = "";
+            bool SMTPIsUseSSL = true;
+            string emailDestination = "tegar.s@weefer.co.id";
+
+            SmtpClient client = new SmtpClient(SMTPServer, SMTPPort);
+            client.Credentials = new System.Net.NetworkCredential(SMTPUser, SMTPPassword);
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = SMTPIsUseSSL;
+
+            var path = Path.Combine(@"wwwroot\images\" + "Logo1.png");
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            {
+                using (var mailMessage = new MailMessage())
+                {
+                    mailMessage.From = new MailAddress(SMTPUser, "no-reply");
+                    mailMessage.To.Add(emailDestination);
+                    mailMessage.IsBodyHtml = true;
+                    mailMessage.Subject = "Cruise Confirmation - " + "Hallo Tegar";
+                    mailMessage.AlternateViews.Add(GetEmbeddedImage(path));
+                    client.Send(mailMessage);
+                }
+            }
         }
-    }
-    public static class BitmapExtension
-    {
-        public static byte[] BitmapToByteArray(this Bitmap bitmap)
+        private AlternateView GetEmbeddedImage(String filePath)
+        {
+            LinkedResource res = new LinkedResource(filePath, new ContentType("image/png"));
+            LinkedResource res2 = new LinkedResource(GenerateBarcode(Guid.NewGuid()), new ContentType("image/Bmp"));
+
+            res.ContentId = Guid.NewGuid().ToString();
+            res2.ContentId = Guid.NewGuid().ToString();
+            string body = "" +
+                "<img src='cid:" + res.ContentId + @"' height='40' /><br/><br/>" +
+                "Dear Sir/Madam " + "Bpk Tegar" + ",<br/><br/>" +
+                "Below are your booking confirmation details:<br/>" +
+                "<table>" +
+                "<tr><td style='width: 120px;'>Vessel</td><td>: " + "dailyBookingPassenger.DailyBooking.Vessel.Name" + "</td></tr>" +
+                "<tr><td>Departure Date</td><td>: " + "BookingDate" + " " + "Timespan" + "</td></tr>" +
+                "<tr><td>Passenger Name</td><td>: " + "Passenger.Name" + "</td></tr>" +
+                "<tr><td>Ic</td><td>: " + "Passenger.ICNo" + "</td></tr>" +
+                "<tr><td>Booked By</td><td>: " + "CreatedUser" + "</td></tr>" +
+                "</table>" +
+                "<br/>Below is your QR Code:<br/>" +
+                "<img src='cid:" + res2.ContentId + @"' height='200' /><br/>" +
+                "addText" + "<br/><br/>" +
+                "Thank you" +
+                "";
+            AlternateView alternateView = AlternateView.CreateAlternateViewFromString(body, null, MediaTypeNames.Text.Html);
+            alternateView.LinkedResources.Add(res);
+            alternateView.LinkedResources.Add(res2);
+            return alternateView;
+        }
+        private Stream GenerateBarcode(Guid ticketId)
+        {
+            QRCodeGenerator QrGenerator = new QRCodeGenerator();
+            QRCodeData QrCodeInfo = QrGenerator.CreateQrCode(ticketId.ToString(), QRCodeGenerator.ECCLevel.Q);
+            QRCode QrCode = new QRCode(QrCodeInfo);
+            Bitmap QrBitmap = QrCode.GetGraphic(60);
+            byte[] BitmapArray = BitmapToByteArray(QrBitmap);
+            MemoryStream logo = new MemoryStream(BitmapArray);
+            return logo;
+        }
+        private byte[] BitmapToByteArray(Bitmap bitmap)
         {
             using (MemoryStream ms = new MemoryStream())
             {
                 bitmap.Save(ms, ImageFormat.Png);
                 return ms.ToArray();
             }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
